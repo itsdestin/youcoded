2bde42874da1a64d309ebe61904c782ee5d041f0
Destin Moss <destinj101@gmail.com>
2026-04-15 03:02:39 -0700

    release: v2.4.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.destin.code"
         minSdk = 28
         targetSdk = 35
-        versionCode = 10
-        versionName = "2.4.2"
+        versionCode = 11
+        versionName = "2.4.3"
     }
 
     signingConfigs {
